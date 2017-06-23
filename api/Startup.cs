51724ebf07b051759f41c45a3c5e0bf54a5d5b97
Cheshire, Christopher W - TS Applications (TSA) <Christopher.Cheshire@denvergov.org>
2017-06-23 17:33:31 -0600

    Added swagger

--- api/Startup.cs
+++ api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.Swagger.Model;
 
 namespace process_improvement_api
 {
@@ -29,6 +30,9 @@ namespace process_improvement_api
         {
             // Add framework services.
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +42,12 @@ namespace process_improvement_api
             loggerFactory.AddDebug();
 
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUi();
         }
     }
 }
