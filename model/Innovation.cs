ab126acfbad55453107a58ddd7786b98b2781bdf
Christopher Cheshire <chris@icebluetech.com>
2017-07-20 09:30:31 -0600

    added props to innovation

--- model/Innovation.cs
+++ model/Innovation.cs
@@ -9,17 +9,28 @@ namespace model
     public class Innovation : IInnovation
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
 
         public int UserId { get; set; }
-        public User User { get; set; }
+        public User Owner { get; set; }
+
+        public DateTime Date { get; set; }
 
         public ICollection<Task> Tasks { get; set; }
 
         public int InnovationTypeId { get; set; }
-        public InnovationType InnovationType { get; set; }
+        public InnovationType Type { get; set; }
 
         public int InnovationCategoryId { get; set; }
-        public InnovationCategory InnovationCategory { get; set; }
+        public InnovationCategory Category { get; set; }
+
+        public ICollection<User> Members { get; set; }
+
+        public string Why { get; set; }
+
+        public string CurrentState { get; set; }
+
+        public string FutureState { get; set; }
+
     }
 }
